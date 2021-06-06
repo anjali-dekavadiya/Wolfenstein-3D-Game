ab0dff504f17a3eea5f4729512051b51eef261cf
Anjali Dekavadiya <anjali.dekavadiya@gmail.com>
2021-06-06 21:47:58 +0530

    Initial commit

---
+++ Assets/Scripts/Enemies/EnemyDeath.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyDeath : MonoBehaviour
+{
+    public int enemyHealth = 10;
+    public bool enemyDead = false;
+    public GameObject enemyAI;
+    public GameObject theEnemy;
+
+    void DamageEnemy(int damageAmount)
+    {
+        enemyHealth -= damageAmount;
+    }
+
+    void Update()
+    {
+        if(enemyHealth <= 0 && enemyDead == false)
+        {
+            enemyDead = true;
+            theEnemy.GetComponent<Animator>().Play("Death");
+            enemyAI.SetActive(false);
+            GlobalScore.scoreValue += 100;
+            GlobalComplete.enemyCount += 1;
+
+        }
+    }
+}
